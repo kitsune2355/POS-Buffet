4f88a75f502819f5535ba1d8c8e11d31ef7d8890
pemika.b <pemika.b@avlgb.com>
2024-10-06 00:17:12 +0700

    theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme/Theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    ,
   </React.StrictMode>
 );
 
---
+++ src/theme/Theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+  typography: {
+    fontFamily: "Arial, sans-serif",
+    fontSize: 14,
+  },
+});
+
+export default theme;
